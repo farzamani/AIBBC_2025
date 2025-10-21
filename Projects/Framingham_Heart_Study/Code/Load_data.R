0d746789cf877c855e4b9c874af7a7b5e682b9bf
Farhad Zamani <zmnfrhd@gmail.com>
2025-10-22 01:53:34 +0200

    remove Project folder and added Challenge folder

--- Projects/Framingham_Heart_Study/Code/Load_data.R
+++
@@ -1,7 +0,0 @@
-# Always ensure the working directory is set to the correct folder
-setwd("Projects/Framingham_Heart_study/")
-
-install.packages("riskCommunicator")
-data("framingham", package="riskCommunicator")
-str(framingham)
-
--- Projects/Gapminder_World_Data/Code/Load_data.R
+++
@@ -1,7 +0,0 @@
-# Always ensure the working directory is set to the correct folder
-setwd("Projects/Gapminder_World_Data/")
-
-
-install.packages("gapminder")  
-library(gapminder)
-d = gapminder 
